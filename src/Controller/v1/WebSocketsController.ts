52bd3f4bf782c6aa0fc8c9356907df105695f4ba
Karol Sójko <karolsojko@gmail.com>
2021-06-21 20:24:00 +0200

    fix: websockets to proxy with connection id in the path

--- src/Controller/v1/WebSocketsController.ts
+++ src/Controller/v1/WebSocketsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { inject } from 'inversify'
-import { BaseHttpController, controller, httpPost } from 'inversify-express-utils'
+import { BaseHttpController, controller, httpDelete, httpPost } from 'inversify-express-utils'
 import { Logger } from 'winston'
 import TYPES from '../../Bootstrap/Types'
 import { HttpServiceInterface } from '../../Service/HttpClientInterface'
@@ -20,6 +20,12 @@ export class WebSocketsController extends BaseHttpController {
       headers: ${JSON.stringify(request.headers)},
       body: ${JSON.stringify(request.body)}`)
 
+    if (!request.headers.connectionid) {
+      response.status(400).send('Missing connection id in the request')
+
+      return
+    }
+
     response.status(200).send('OK')
   }
 
@@ -29,6 +35,27 @@ export class WebSocketsController extends BaseHttpController {
       headers: ${JSON.stringify(request.headers)},
       body: ${JSON.stringify(request.body)}`)
 
-    await this.httpService.callAuthServer(request, response, 'sockets', request.body)
+    if (!request.headers.connectionid) {
+      response.status(400).send('Missing connection id in the request')
+
+      return
+    }
+
+    await this.httpService.callAuthServer(request, response, `sockets/${request.headers.connectionid}`, request.body)
+  }
+
+  @httpDelete('/')
+  async deleteWebSocketConnection(request: Request, response: Response): Promise<void> {
+    this.logger.debug(`WebSockets connection removing request:
+      headers: ${JSON.stringify(request.headers)},
+      body: ${JSON.stringify(request.body)}`)
+
+    if (!request.headers.connectionid) {
+      response.status(400).send('Missing connection id in the request')
+
+      return
+    }
+
+    await this.httpService.callAuthServer(request, response, `sockets/${request.headers.connectionid}`, request.body)
   }
 }
