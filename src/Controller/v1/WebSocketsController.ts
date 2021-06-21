64a18eb7be3dd1da6e760c3e45623f88050a41b7
Karol Sójko <karolsojko@gmail.com>
2021-06-21 19:49:48 +0200

    feat: add debug logs to websockets controller

--- src/Controller/v1/WebSocketsController.ts
+++ src/Controller/v1/WebSocketsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { inject } from 'inversify'
 import { BaseHttpController, controller, httpPost } from 'inversify-express-utils'
+import { Logger } from 'winston'
 import TYPES from '../../Bootstrap/Types'
 import { HttpServiceInterface } from '../../Service/HttpClientInterface'
 
@@ -8,17 +9,26 @@ import { HttpServiceInterface } from '../../Service/HttpClientInterface'
 export class WebSocketsController extends BaseHttpController {
   constructor(
     @inject(TYPES.HTTPService) private httpService: HttpServiceInterface,
+    @inject(TYPES.Logger) private logger: Logger,
   ) {
     super()
   }
 
   @httpPost('/connect')
-  async connect(_request: Request, response: Response): Promise<void> {
+  async connect(request: Request, response: Response): Promise<void> {
+    this.logger.debug(`WebSockets connect request:
+      headers: ${JSON.stringify(request.headers)},
+      body: ${JSON.stringify(request.body)}`)
+
     response.status(200).send('OK')
   }
 
   @httpPost('/')
   async createWebSocketConnection(request: Request, response: Response): Promise<void> {
+    this.logger.debug(`WebSockets connection persisting request:
+      headers: ${JSON.stringify(request.headers)},
+      body: ${JSON.stringify(request.body)}`)
+
     await this.httpService.callAuthServer(request, response, 'sockets', request.body)
   }
 }
