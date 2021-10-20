42efbfecf42fa01cae63bd28595c466919d112d7
Karol Sójko <karolsojko@protonmail.com>
2021-10-20 13:51:01 +0200

    feat: add cancelling and updating subscriptions with subscription token endpoints

--- src/Controller/v2/PaymentsControllerV2.ts
+++ src/Controller/v2/PaymentsControllerV2.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { BaseHttpController, controller, httpGet } from 'inversify-express-utils'
+import { BaseHttpController, controller, httpDelete, httpGet, httpPatch } from 'inversify-express-utils'
 import { inject } from 'inversify'
 import TYPES from '../../Bootstrap/Types'
 import { HttpServiceInterface } from '../../Service/HttpClientInterface'
@@ -13,7 +13,17 @@ export class PaymentsControllerV2 extends BaseHttpController {
   }
 
   @httpGet('/subscriptions')
-  async subscriptions(request: Request, response: Response): Promise<void> {
+  async getSubscriptionsWithFeatures(request: Request, response: Response): Promise<void> {
     await this.httpService.callPaymentsServer(request, response, 'api/subscriptions/features', request.body)
+  }
+
+  @httpDelete('/subscriptions/:subscriptionId', TYPES.SubscriptionTokenAuthMiddleware)
+  async cancelSubscription(request: Request, response: Response): Promise<void> {
+    await this.httpService.callPaymentsServer(request, response, `api/subscriptions/${request.params.subscriptionId}`, request.body)
+  }
+
+  @httpPatch('/subscriptions/:subscriptionId', TYPES.SubscriptionTokenAuthMiddleware)
+  async updateSubscription(request: Request, response: Response): Promise<void> {
+    await this.httpService.callPaymentsServer(request, response, `api/subscriptions/${request.params.subscriptionId}`, request.body)
   }
 }
