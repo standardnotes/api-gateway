595c5eecc1387b5d4172eaef42b757c86c661586
Karol Sójko <karolsojko@protonmail.com>
2022-01-06 15:58:57 +0100

    feat: add generating usage report

---
+++ bin/report.ts
@@ -0,0 +1,63 @@
+import 'reflect-metadata'
+
+import 'newrelic'
+
+import { Logger } from 'winston'
+
+import { ContainerConfigLoader } from '../src/Bootstrap/Container'
+import TYPES from '../src/Bootstrap/Types'
+import { Env } from '../src/Bootstrap/Env'
+import { DomainEventPublisherInterface, DailyVersionAdoptionReportGeneratedEvent } from '@standardnotes/domain-events'
+import { AnalyticsStoreInterface } from '../src/Service/AnalyticsStoreInterface'
+
+
+const requestReport = async (
+  analyticsStore: AnalyticsStoreInterface,
+  domainEventPublisher: DomainEventPublisherInterface,
+): Promise<void> => {
+  const event: DailyVersionAdoptionReportGeneratedEvent = {
+    type: 'DAILY_VERSION_ADOPTION_REPORT_GENERATED',
+    createdAt: new Date(),
+    meta: {
+      correlation: {
+        userIdentifier: '',
+        userIdentifierType: 'uuid',
+      },
+    },
+    payload: {
+      applicationStatistics: await analyticsStore.getYesterdayApplicationUsage(),
+      snjsStatistics: await analyticsStore.getYesterdaySNJSUsage(),
+    },
+  }
+
+  await domainEventPublisher.publish(event)
+}
+
+const container = new ContainerConfigLoader
+void container.load().then(container => {
+  const env: Env = new Env()
+  env.load()
+
+  const logger: Logger = container.get(TYPES.Logger)
+
+  logger.info('Starting usage report generation...')
+
+  const analyticsStore: AnalyticsStoreInterface = container.get(TYPES.AnalyticsStore)
+  const domainEventPublisher: DomainEventPublisherInterface = container.get(TYPES.DomainEventPublisher)
+
+  Promise
+    .resolve(requestReport(
+      analyticsStore,
+      domainEventPublisher,
+    ))
+    .then(() => {
+      logger.info('Usage report generation complete')
+
+      process.exit(0)
+    })
+    .catch((error) => {
+      logger.error(`Could not finish usage report generation: ${error.message}`)
+
+      process.exit(1)
+    })
+})
--- src/Bootstrap/Types.ts
+++ src/Bootstrap/Types.ts
@@ -2,6 +2,7 @@ const TYPES = {
   Logger: Symbol.for('Logger'),
   Redis: Symbol.for('Redis'),
   HTTPClient: Symbol.for('HTTPClient'),
+  SNS: Symbol.for('SNS'),
   // env vars
   SYNCING_SERVER_JS_URL: Symbol.for('SYNCING_SERVER_JS_URL'),
   AUTH_SERVER_URL: Symbol.for('AUTH_SERVER_URL'),
@@ -9,6 +10,9 @@ const TYPES = {
   AUTH_JWT_SECRET: Symbol.for('AUTH_JWT_SECRET'),
   HTTP_CALL_TIMEOUT: Symbol.for('HTTP_CALL_TIMEOUT'),
   VERSION: Symbol.for('VERSION'),
+  SNS_TOPIC_ARN: Symbol.for('SNS_TOPIC_ARN'),
+  SNS_AWS_REGION: Symbol.for('SNS_AWS_REGION'),
+  REDIS_EVENTS_CHANNEL: Symbol.for('REDIS_EVENTS_CHANNEL'),
   // Middleware
   AnalyticsMiddleware: Symbol.for('AnalyticsMiddleware'),
   AuthMiddleware: Symbol.for('AuthMiddleware'),
@@ -16,6 +20,7 @@ const TYPES = {
   // Services
   HTTPService: Symbol.for('HTTPService'),
   AnalyticsStore: Symbol.for('AnalyticsStore'),
+  DomainEventPublisher: Symbol.for('DomainEventPublisher'),
 }
 
 export default TYPES
