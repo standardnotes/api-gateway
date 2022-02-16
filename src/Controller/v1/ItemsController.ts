190d0e8097ddd7135861549c2233fa7c954411a2
Karol Sójko <karolsojko@protonmail.com>
2022-02-16 13:58:24 +0100

    feat: add checking integrity and fetching single item endpoints

--- src/Controller/v1/ItemsController.ts
+++ src/Controller/v1/ItemsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { inject } from 'inversify'
-import { BaseHttpController, controller, httpPost } from 'inversify-express-utils'
+import { BaseHttpController, controller, httpGet, httpPost } from 'inversify-express-utils'
 import TYPES from '../../Bootstrap/Types'
 import { HttpServiceInterface } from '../../Service/HttpClientInterface'
 
@@ -15,5 +15,15 @@ export class ItemsController extends BaseHttpController {
   @httpPost('/')
   async sync(request: Request, response: Response): Promise<void> {
     await this.httpService.callSyncingServer(request, response, 'items/sync', request.body)
+  }
+
+  @httpPost('/check-integrity')
+  async checkIntegrity(request: Request, response: Response): Promise<void> {
+    await this.httpService.callSyncingServer(request, response, 'items/check-integrity', request.body)
+  }
+
+  @httpGet('/:uuid')
+  async getItem(request: Request, response: Response): Promise<void> {
+    await this.httpService.callSyncingServer(request, response, `items/${request.params.uuid}`, request.body)
   }
 }
